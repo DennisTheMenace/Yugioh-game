9621b52f5c25a49520aaa4fa7da1ec8c177b5072
Joshua Rowe <joshua@wired.com.au>
2011-08-15 19:22:55 +1000

    Added the battle command to the game

---
+++ Battle.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+def battle
+  playerset
+  fieldmonsters = []
+  efieldmonsters = []
+  @@monsterfield.each { |x| 
+  if x[:type] == "monster"
+    fieldmonsters.push(x)
+  end
+  }
+  if fieldmonsters.empty? == true	
+    puts "There are no monsters to battle.".red
+    return
+  end
+  @@emonsterfield.each { |x| 			
+  if x[:type] == "monster"
+    efieldmonsters.push(x)
+  end
+  }
+  if efieldmonsters.empty? == true	
+    puts "There are no monsters to battle.".red
+    return
+  end
+  puts "What monster do you choose?"
+  fieldmonsters.each {|x| puts x[:name]}
+  response1 = gets.to_i
+  response1 -= 1
+  puts "You have selected #{@@monsterfield[response1][:name]}"
+  puts "What monster do you want to battle?"
+  efieldmonsters.each {|x| puts x[:name]}
+  response2 = gets.to_i
+  response2 -= 1
+  
+  #answer = 200
+  #answer = 1000 - 800
+  
+  answer = @@monsterfield[response1][:atk] - @@emonsterfield[response2][:atk]
+  puts answer
+  if answer == 0
+    puts "Draw"
+    @@graveyard.push(@@monsterfield[response1])
+    @@monsterfield.delete(@@monsterfield[response1])
+    @@egraveyard.push(@@emonsterfield[response2])
+    @@emonsterfield.delete(@@emonsterfield[response2])
+    $file.puts("#{@@name} drew in a battle against #{@@ename}")
+  end
+  if answer > 0
+    puts "Your monster won the battle"
+    $file.puts("#{@@name} won a battle against #{@@ename}")
+  end
+end
--- Commands.rb
+++ Commands.rb
@@ -1,38 +1,43 @@
 #!/usr/bin/env ruby
 
+# Basically, the valid input commands list
+
 def command(player)
     playerset
     response = gets.chomp
-    if response == "smn"
-      summon($hand1)
+    if response == "smn"	# Summon a monster
+      summon
     elsif response == "spl"
-      spell("#{$activeplayer}")
+      spell		# Remind me to update spell to allow for inputting $activeplayer
     elsif response == "mf"
       puts 'Monster field:'
-      puts $monsterfield[0][:name]
+      @@monsterfield.each {|x| puts x[:name]}
       #$monsterfield.each { |b| puts $monsterfield[b]}
-    elsif response == "gv"
+    elsif response == "gv"		# Lists all cards in your graveyard
       puts 'Graveyard:'
       list(@@graveyard)
       #$graveyard.each { |b| puts $graveyard[b][:name]}
     elsif response == "lp"
       puts "You have #{$lp}"
     elsif response == "hc"
-      puts "You have #{$hand1.size} cards in your hand"
+      puts "You have #{@@hand.size} cards in your hand"
+    
     elsif response == "trp"					# Not implemented yet.
       puts "This has not been implemented yet"
-    elsif response == "exit"
+    elsif response == "btl"
+      battle
+    elsif response == "exit"		# Allows exiting and closes all open files.
       $file.close
       sleep 1
       puts "Files flushed properly"
       sleep 1
       abort("Exiting")
-    elsif response == "turn"
+    elsif response == "turn"	# Outputs the current players turn
       puts $activeplayer
-    elsif response == "help"
+    elsif response == "help"	# The word 'Help' says it all
       help
     # Testing commands:
-    elsif response == "rpt"
+    elsif response == "rpt" # Forgot what this was suppose to be =(
       puts "Not ready yet".yellow
     
     else
