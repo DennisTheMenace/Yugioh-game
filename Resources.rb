38ce329824254b4d0c47bf1333262f4c29c78e90
Joshua Rowe <joshua@wired.com.au>
2011-08-16 13:14:16 +1000

    Updated the resources list

--- Resources.rb
+++ Resources.rb
@@ -3,7 +3,7 @@
 # Imports all the required files.
 
 require_relative 'Help.rb' # More help coming soon...
-require_relative 'Deck.rb'
+require_relative 'Deck.rb' # Initialises your deck
 require_relative 'Draw.rb'
 require_relative 'Spell.rb'
 require_relative 'Field.rb'  # Sets all the field variables
@@ -13,7 +13,7 @@ require_relative 'Phases.rb'
 require_relative 'Update.rb'
 require_relative 'Colourtest.rb'
 require_relative 'Commands.rb'
-require_relative 'Return.rb' # In development
+#require_relative 'Return.rb' # In development
 require_relative 'Playerset.rb'
 require_relative 'Trap.rb' # Totally useless
 require_relative 'List.rb'
