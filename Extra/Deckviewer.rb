4e72b1414006e99c7578ed8c8c9add90bb9aff37
Joshua Rowe <joshua@wired.com.au>
2011-10-08 17:44:11 +1100

    Added a deckviewer utility. Requires Rubygame

---
+++ Extra/Deckviewer.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require "rubygame"
+include Rubygame
+include Rubygame::Events
+
+require_relative 'Deck.rb'
+file = $deck1[0][:file]
+# 
+$number = 0
+begin
+  image = Surface.load( file )
+  screen = Screen.open( image.size )
+  screen.title = File.basename(file)
+end
+class MyHandler
+  include Rubygame::EventHandler::HasEventHandler
+end
+ 
+handler = MyHandler.new
+handler.make_magic_hooks( :q            => :exit_script,
+                          :escape       => :exit_script,
+                          :mouse_right  => :exit_script,
+                          QuitRequested => :exit_script )
+ 
+def exit_script
+  exit
+end
+
+loop do
+  events = Rubygame::Events.fetch_sdl_events
+  events.each do |event|
+    handler.handle( event )
+  end
+  image = Surface.load( $deck1[$number][:file] )
+  image.blit( screen, [0,0] )
+ 
+  screen.update
+ 
+  sleep 1
+  $number += 1
+ 
+end
