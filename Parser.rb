7e2fcb54925b48f595ec6827bc3c99dc3095c79c
Joshua Rowe <joshua@wired.com.au>
2012-01-28 15:35:41 +1100

    Added the experimental version of the parser

---
+++ Parser.rb
@@ -0,0 +1,88 @@
+#!/usr/bin/env ruby
+
+# In Resources.rb? - No
+
+# A card effect parser
+
+# Note - Put a space in front of a . (full stop)
+
+def is_number?(i)
+  true if Float(i) rescue false
+end
+
+
+def parse(string)
+  #string = string.to_s
+  string = string.downcase
+  array = string.split(/ /)
+  
+  #puts array
+  array.each {|x|
+
+  # Draw
+
+  #puts x
+  #puts "Checking if it is == 'draw'"
+  if x == 'draw'
+    #puts "Equals draw"
+    #sleep 0.4
+    b = array.index x
+    #puts b
+    a = b + 1
+    if is_number?(array[a]) == true
+      
+      sleep 0.3
+      puts "true"
+      array[a] = array[a].to_i
+      sleep 0.3
+      array[a].times do draw($activeplayer,'yes', false) end
+    end
+  end
+
+  # Discard
+  
+  if x == 'discard'
+    b = array.index x
+    a = b + 1
+    if is_number?(array[a]) == true
+      c = a + 1
+      puts "The next word is..."
+      puts array[c]
+      if array[c] == "card"
+        puts "type = all"
+        type = 'all'
+      elsif array[c] == 'monster'
+        type = 'monster'
+      elsif array[c] == 'spell'
+        type = 'spell'
+      elsif array[c] == 'trap'
+        type = 'trap'
+      end
+      array[a] = array[a].to_i
+      #puts type
+      type = 'all'
+      array[a].times do discard(type) end
+    end
+
+
+
+  end
+
+  # Destroy
+
+  if x == 'destroy'
+    b = array.index x
+    a = b + 1
+    if is_number?(array[a]) == true
+      # More to come...
+
+
+
+    end
+  end
+
+  
+}
+  
+end
+
